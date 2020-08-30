0b154e84292e1037d2015aebd2194aacc49a8633
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-08-30 18:36:11 +0530

    Add files via upload

---
+++ amstrong.c
@@ -0,0 +1,35 @@
+/*
+ * amstrong.c
+ *
+ *  Created on: Aug 30, 2020
+ *      Author: HP
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int number, sum=0, rem=0,tempNumber;
+
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+
+    tempNumber=number;
+
+    while(tempNumber!=0)
+    {
+        rem=tempNumber%10;
+        sum=sum + (rem*rem*rem);
+        tempNumber/=10;
+    }
+
+
+    if(sum==number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
+}
+
+
