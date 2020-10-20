a72cc5515461868216375959833e6207587bd4c5
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-10-20 13:27:50 +0530

    Add files via upload

---
+++ python pratices/sum.py
@@ -0,0 +1,9 @@
+nums = [23, 22, 44, 17, 77, 55, 1, 65, 82, 2]
+num_min = min(nums)
+print(num_min)
+nums = [23, 22, 44, 17, 77, 55, 1, 65, 82, 2]
+num_max = max(nums)
+print(num_max)
+nums = [23, 22, 44, 17, 77, 55, 1, 65, 82, 2]
+total_num = sum(nums)
+print(total_num)
