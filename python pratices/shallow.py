852b2ada0ebe950f961a09450d0964a9b006999f
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-10-20 22:15:25 +0530

    Add files via upload

---
+++ python pratices/shallow.py
@@ -0,0 +1,13 @@
+my_list=[[2,3,4],['a','b','c']]
+
+print('id of my_list',hex(id(my_list)))
+
+new_list=my_list
+
+print('id of new_list',hex(id(my_list)))
+
+new_list.append([7,9,8])
+
+print('new list',new_list)
+
+print('my_list',my_list)
