9efdad67dbf5101f43c0d3c0acb72370a2cd202f
nainachavhan <70107086+nainachavhan@users.noreply.github.com>
2020-10-23 17:14:55 +0530

    Add files via upload

---
+++ python pratices/perfectNo.py
@@ -0,0 +1,13 @@
+def perfect_number(n):
+    
+   sum = 0
+    
+   for x in range(1, n):
+     
+     if n % x == 0:
+           
+         sum += x
+    
+   return sum == n
+
+print(perfect_number(28))
